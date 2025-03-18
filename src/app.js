38254125351d5d8d830354280fbece15974ec839
Airat Vagapov <airlake@yandex.ru>
2025-03-18 23:59:24 +0300

    db config

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
-const express = require('express')
+const express = require("express");
+const client = require("./config/db");
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200)
-    res.send('test my simple API')
-})
+app.get("/", (req, res) => {
+    res.status(200);
+    res.send("test my simple API");
+});
 
-module.exports = app
+async function startServer() {
+    try {
+        await client.connect();
+        console.log('Connected to PostgreSQL');
+
+        const result = await client.query("SELECT NOW()");
+        console.log("Connected to PostgreSQL at:", result.rows[0].now);
+
+        const port = process.env.PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    } catch (err) {
+        console.error("Error connecting to PostgreSQL:", err);
+        console.log(err)
+        process.exit(1); // Завершаем процесс, если не удалось подключиться к базе данных
+    }
+}
+
+startServer();
+
+module.exports = app;
